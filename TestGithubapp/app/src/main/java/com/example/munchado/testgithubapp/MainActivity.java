29b2b9194cd80a1c25d8a94d1884c22d1f6575a0
pkhare-munchado <pkhare@munchao.in>
2016-09-12 19:38:01 +0530

    Life cycle callback test : calling finish on Create

--- TestGithubapp/app/src/main/java/com/example/munchado/testgithubapp/MainActivity.java
+++ TestGithubapp/app/src/main/java/com/example/munchado/testgithubapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.munchado.testgithubapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +12,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(MainActivity.this,"Hello Piyush khare updated this toast.",Toast.LENGTH_SHORT).show();
+        Log.d("=====","========== onCreate");
+
+        finish();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("=====","========== onStart");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("=====","========== onRestart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("=====","========== onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("=====","========== onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("=====","========== onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("=====","========== onDestroy");
     }
 }
